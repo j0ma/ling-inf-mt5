139f12b23e6a0e9159a1372125586092a4be5fec
Jonne Saleva <jonne@jonnesaleva.com>
2023-04-01 16:01:03 -0400

    Add FLORES-200 and NTREX data

--- click_finetune_mt5.py
+++ click_finetune_mt5.py
@@ -1,9 +1,10 @@
 import click
+import evaluate
+import numpy as np
 from datasets import Dataset
 from transformers import (DataCollatorForSeq2Seq, MT5ForConditionalGeneration,
                           MT5Tokenizer, Seq2SeqTrainer,
-                          Seq2SeqTrainingArguments, Trainer, TrainingArguments,
-                          default_data_collator)
+                          Seq2SeqTrainingArguments, default_data_collator)
 
 
 @click.command()
@@ -59,6 +60,13 @@ from transformers import (DataCollatorForSeq2Seq, MT5ForConditionalGeneration,
 @click.option(
     "--max-length-tokens", type=int, default=128, help="Maximum number of tokens"
 )
+@click.option("--save-total-limit", type=int, default=5)
+@click.option("--learning-rate", type=float, default=5e-5)
+@click.option("--save-steps", help="Save every `save-steps` steps.", default=100)
+@click.option("--eval-steps", help="Eval every `eval-steps` steps.", default=100)
+@click.option("--warmup-steps", default=100)
+@click.option("--logging-steps", default=50)
+@click.option("--predict-with-generate", is_flag=True)
 def train_mt5(
     source_path,
     target_path,
@@ -70,7 +78,13 @@ def train_mt5(
     max_steps,
     batch_size,
     learning_rate,
-    max_length_tokens
+    max_length_tokens,
+    save_total_limit,
+    save_steps,
+    eval_steps,
+    warmup_steps,
+    logging_steps,
+    predict_with_generate,
 ):
     # Load the MT5 tokenizer and model
     tokenizer = MT5Tokenizer.from_pretrained(model_name)
@@ -84,7 +98,9 @@ def train_mt5(
             target_lines = [line.strip() for line in f]
 
         # Create a dataset with the source and target sentences
-        dataset = Dataset.from_dict({source_lang: source_lines, target_lang: target_lines})
+        dataset = Dataset.from_dict(
+            {source_lang: source_lines, target_lang: target_lines}
+        )
 
         return dataset
 
@@ -105,6 +121,38 @@ def train_mt5(
     train_data = data["train"]
     test_data = data["test"]
 
+    # SacreBLEU
+    metric = evaluate.load("sacrebleu")
+
+    def postprocess_text(preds, labels):
+        preds = [pred.strip() for pred in preds]
+        labels = [[label.strip()] for label in labels]
+
+        return preds, labels
+
+    def compute_metrics(eval_preds):
+        preds, labels = eval_preds
+
+        if isinstance(preds, tuple):
+            preds = preds[0]
+        decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+
+        labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+        decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+        decoded_preds, decoded_labels = postprocess_text(decoded_preds, decoded_labels)
+
+        result = metric.compute(predictions=decoded_preds, references=decoded_labels)
+        result = {"bleu": result["score"]}
+
+        prediction_lens = [
+            np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds
+        ]
+        result["gen_len"] = np.mean(prediction_lens)
+        result = {k: round(v, 4) for k, v in result.items()}
+
+        return result
+
     # Define the training arguments
     training_args = Seq2SeqTrainingArguments(
         output_dir=output_dir,
@@ -112,14 +160,14 @@ def train_mt5(
         per_device_train_batch_size=batch_size,
         per_device_eval_batch_size=batch_size,
         evaluation_strategy="steps",
-        save_total_limit=1,
+        save_total_limit=save_total_limit,
         learning_rate=learning_rate,
-        save_steps=10,
-        eval_steps=10,
-        warmup_steps=0,
-        logging_steps=1,
+        save_steps=save_steps,
+        eval_steps=eval_steps,
+        warmup_steps=warmup_steps,
+        logging_steps=logging_steps,
         overwrite_output_dir=True,
-        predict_with_generate=True,
+        predict_with_generate=predict_with_generate,
     )
 
     data_collator = DataCollatorForSeq2Seq(tokenizer=tokenizer, model=model)
@@ -131,6 +179,7 @@ def train_mt5(
         eval_dataset=test_data,
         data_collator=data_collator,
         tokenizer=tokenizer,
+        compute_metrics=compute_metrics
     )
 
     # Train the model
---
+++ side_by_side.py
@@ -0,0 +1,22 @@
+import click
+import json
+
+
+@click.command()
+@click.option("-k1")
+@click.option("-k2")
+@click.option("--sep", "-s", default="\t")
+def sidebyside(k1, k2, sep):
+
+    stdin = click.get_text_stream("stdin")
+
+    def json_line(line1, line2):
+        return json.dumps({k1: line1, k2: line2})
+
+    for line in stdin:
+
+        line1, line2 = line.strip().split(sep)
+        click.echo(json_line(line1, line2), nl=True)
+    
+if __name__ == "__main__":
+    sidebyside()
