ee0529f85d2f29226c0e8747ba27196a8664127d
Jonne Saleva <jonne@jonnesaleva.com>
2023-04-01 17:22:36 -0400

    Move to --corpus-name argument.  Change `--get-rid-of-orth-flores` to `--get-rid-of-orth`.

--- create_data.py
+++ create_data.py
@@ -19,19 +19,27 @@ import datasets as ds
     required=True,
     help="Path to the folder where the train and test datasets will be saved.",
 )
+@click.option("--corpus-name", type=click.Choice(["flores200", "ntrex"]), required=True)
 @click.option(
     "--test-size",
     type=float,
     default=0.2,
     help="Fraction of the data to be used for testing.",
 )
-@click.option("--flores", is_flag=True)
-@click.option("--get-rid-of-orth-flores", is_flag=True)
+@click.option("--get-rid-of-orth", is_flag=True)
 def create_dataset(
-    data_folder, output_folder, test_size, flores=False, get_rid_of_orth_flores=False
+    data_folder,
+    output_folder,
+    corpus_name,
+    test_size,
+    get_rid_of_orth=False,
 ):
     # Get a list of all the parallel data files in the data folder
-    file_names = list(Path(data_folder).glob("*.dev"))
+    file_names = list(
+        Path(data_folder).glob(
+            "*.dev" if corpus_name.startswith("flores") else "newstest*2019*.txt"
+        )
+    )
 
     # Load the data from each file into a list of dictionaries
     data = ds.DatasetDict()
@@ -41,13 +49,20 @@ def create_dataset(
 
         return d
 
+    FLORES_MODE = corpus_name.startswith("flores")
+
     for file_name in file_names:
         with open(file_name, "r", encoding="utf-8") as f:
             sentences = [line.strip() for line in f]
-            lang = file_name.name.replace(".dev", "")
 
-            if flores and get_rid_of_orth_flores:
-                lang = lang.split("_")[0]
+            if FLORES_MODE:
+                lang = file_name.name.replace(".dev", "")
+            else:
+                lang = file_name.name.split(".")[1]
+
+            sep = "_" if FLORES_MODE else "-"
+            lang = lang.split(sep)[0] if get_rid_of_orth else lang
+
             data[lang] = ds.Dataset.from_dict({"text": sentences}).map(
                 functools.partial(add_lang, lang=lang)
             )
