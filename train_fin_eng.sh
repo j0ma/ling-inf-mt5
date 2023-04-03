3f22cde1510117e1c53fcb6c9a399aeba302b7b9
Jonne Sälevä <jonnesaleva@brandeis.edu>
2023-04-03 13:41:05 -0400

    small edits to train script

--- train_fin_eng.sh
+++ train_fin_eng.sh
@@ -1,15 +1,18 @@
 #!/usr/bin/env bash
 
-CUDA_VISIBLE_DEVICES="0" python click_finetune_mt5.py \
+num_gpus=$(nvidia-smi -L | wc -l)
+
+python click_finetune_mt5.py \
 	--flores-path ./data-bin/flores-dev-no-orth/ \
 	--ntrex-path ./data-bin/ntrex-no-orth/ \
 	--model-name "google/mt5-small" \
 	--source-lang fin \
 	--target-lang eng \
 	--finetune-langs 'fin' \
-	--num-train-epochs 20 \
-	--batch-size 32 \
-    --predict-with-generate
-	#--logging-steps 500 \
-	#--warmup-steps 500 \
-	#--logging-steps 500 \
+	--num-train-epochs 600 \
+	--batch-size 20 \
+    --predict-with-generate \
+    --logging-steps 600 \
+    --warmup-steps 600 \
+    --logging-steps 600 \
+    --num-gpus "${num_gpus}"
