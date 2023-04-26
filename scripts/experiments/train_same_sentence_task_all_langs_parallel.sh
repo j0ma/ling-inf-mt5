cccd389434ee8eeb506d75b4b275972f5d3a9ea9
Jonne Saleva <jonnesaleva@brandeis.edu>
2023-04-26 18:24:11 -0400

    Run single sbatch job from a bash function

--- scripts/experiments/train_same_sentence_task_all_langs_parallel.sh
+++ scripts/experiments/train_same_sentence_task_all_langs_parallel.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
 
-SINGLE_EXP_SBATCH_CMD="scripts/experiments/sbatch_train_same_sentence_task.sh"
-parallel "SAME_SENTENCE_TASK_LANG={1} sbatch -J {1}-samesent ${SINGLE_EXP_SBATCH_CMD}" :::: ./data/all_languages.txt
+set -euo pipefail
+
+function run_single_sbatch () {
+    local lang=${1}
+    export SAME_SENTENCE_TASK_LANG=${lang}
+    job_name=${lang}-samesent
+
+    sbatch \
+        -J "${job_name}" \
+        scripts/experiments/sbatch_train_same_sentence_task.sh
+}
+
+export -f run_single_sbatch
+
+parallel "run_single_sbatch {1}" :::: ./data/all_languages.txt
