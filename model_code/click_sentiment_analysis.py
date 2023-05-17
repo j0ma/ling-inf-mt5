61d789e8f36a149489fecac79c0b046271a49b88
Jonne Sälevä <jonnesaleva@brandeis.edu>
2023-05-17 16:13:45 -0400

    Fixes to nusa data creation and sentiment analysis script

--- model_code/click_sentiment_analysis.py
+++ model_code/click_sentiment_analysis.py
@@ -1,15 +1,14 @@
 import click
-import numpy as np
-from transformers import (
-    AutoTokenizer,
-    AutoModelForSequenceClassification,
-    TrainingArguments,
-    Trainer,
-    DataCollatorWithPadding,
-)
 import datasets as ds
+import evaluate
+import numpy as np
+from transformers import (AutoModelForSequenceClassification, AutoTokenizer,
+                          DataCollatorWithPadding, Trainer, TrainingArguments)
 
-MAX_LENGTH_TOKENS=128
+MAX_LENGTH_TOKENS = 128
+
+f1 = evaluate.load("f1")
+
 
 @click.command()
 @click.option("--dataset-path", type=click.Path(exists=True))
@@ -21,29 +20,27 @@ MAX_LENGTH_TOKENS=128
 @click.option("--num-epochs", default=2, help="Number of epochs to train the model for")
 @click.option("--batch-size", default=16, help="Batch size for training")
 def main(dataset_path, model_name, num_epochs, batch_size):
-
     # Initialize the tokenizer and the model
     label_to_id = {"negative": 0, "neutral": 1, "positive": 2}
-    tokenizer = AutoTokenizer.from_pretrained(model_name, padding=True, truncation=True, max_length=MAX_LENGTH_TOKENS)
-    model = AutoModelForSequenceClassification.from_pretrained(model_name, num_labels=len(label_to_id))
+    id_to_label = {v: k for k, v in label_to_id.items()}
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_name, padding=True, truncation=True, max_length=MAX_LENGTH_TOKENS
+    )
+    model = AutoModelForSequenceClassification.from_pretrained(
+        model_name, num_labels=len(label_to_id)
+    )
 
     # Load the dataset and preprocess it
     dataset_dict: ds.DatasetDict = ds.load_from_disk(dataset_path)
-    new_dataset_dict = ds.DatasetDict()
-    for lang, _ds in dataset_dict.items():
-        # split to train and test sets with 20% in test
-        ds_with_split = _ds.train_test_split(test_size=0.2).rename_column("label", "labels")
-        new_dataset_dict[lang] = ds_with_split
-
-    dataset_dict = new_dataset_dict
 
     # Create the training and validation datasets
-    train_dataset = ds.concatenate_datasets([dataset_dict[lang]["train"] for lang in dataset_dict])
-    test_dataset = ds.concatenate_datasets([dataset_dict[lang]["test"] for lang in dataset_dict])
+    train_dataset = dataset_dict['train']
+    test_dataset = dataset_dict['test']
 
     def preprocess_function(examples):
         inputs = examples["text"]
-        labels = [label_to_id[label] for label in examples["labels"]]
+        labels = [label_to_id[label] for label in examples["label"]]
+        del examples['label']
 
         model_inputs = tokenizer(
             inputs, padding=True, truncation=True, max_length=MAX_LENGTH_TOKENS
@@ -59,18 +56,39 @@ def main(dataset_path, model_name, num_epochs, batch_size):
     def compute_metrics(eval_pred):
         predictions, labels = eval_pred
         predictions = np.argmax(predictions, axis=1)
-        return {"accuracy": (predictions == labels).mean()}
+
+        metrics = {
+            "macro_f1": f1.compute(
+                predictions=predictions, references=labels, average="macro"
+            )["f1"],
+            "micro_f1": f1.compute(
+                predictions=predictions, references=labels, average="micro"
+            )["f1"],
+        }
+
+        # Per-class F1s
+        class_f1s = f1.compute(
+            predictions=predictions, references=labels, average=None
+        )["f1"]
+        class_ids = sorted(set(predictions) | set(labels))
+        f1s_per_class = {
+            f"f1_{id_to_label[class_id]}": f1_score
+            for class_id, f1_score in zip(class_ids, class_f1s)
+        }
+        metrics.update(f1s_per_class)
+
+        return metrics
 
     data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
     # Set training arguments
     training_args = TrainingArguments(
-        output_dir="./results",
+        output_dir="./sentiment_results",
         num_train_epochs=num_epochs,
         per_device_train_batch_size=batch_size,
         per_device_eval_batch_size=batch_size,
         evaluation_strategy="epoch",
-        logging_dir="./logs",
+        logging_dir="./sentiment_logs",
         overwrite_output_dir=True,
     )
 
@@ -84,7 +102,6 @@ def main(dataset_path, model_name, num_epochs, batch_size):
         compute_metrics=compute_metrics,
     )
 
-    import pudb; pudb.set_trace()
     trainer.train()
     metrics = trainer.evaluate()
 
--- scripts/data/create_data_nusax.py
+++ scripts/data/create_data_nusax.py
@@ -14,7 +14,8 @@ import datasets as ds
     help="Path to the folder containing the parallel data files.",
 )
 @click.option(
-    "--output-folder", type=click.Path(),
+    "--output-folder",
+    type=click.Path(),
     required=True,
     help="Path to the folder where the train and test datasets will be saved.",
 )
@@ -22,19 +23,33 @@ def create_dataset(
     data_folder,
     output_folder,
 ):
-
     def add_lang(d, lang="fin"):
         d["language"] = lang
 
         return d
 
-    data = ds.DatasetDict()
-    language_subfolders = list(p for p in Path(data_folder).glob("*") if p.is_dir())
-    for lang_sf in language_subfolders:
-        csv_files = list(str(p) for p in lang_sf.glob("*.csv"))
-        lang_dataset = ds.load_dataset('csv', data_files=csv_files).map(add_lang)
-        data[lang_sf.name] = lang_dataset['train']
+    # data = ds.DatasetDict()
+    from collections import defaultdict
+    datas = defaultdict(list)
 
+    language_subfolders = list(p for p in Path(data_folder).glob("*") if p.is_dir())
+    from functools import partial
+
+    for lang_sf in language_subfolders:
+        language = lang_sf.name
+        csv_files = list(str(p) for p in lang_sf.glob("*.csv"))
+        _add_lang = partial(add_lang, lang=language)
+        lang_dataset = ds.load_dataset(
+            "csv", data_files=dict(zip(["train", "valid", "test"], csv_files))
+        ).map(_add_lang)
+
+        for split in ["train", "valid", "test"]:
+            datas[split].append(lang_dataset[split])
+
+    data = ds.DatasetDict()
+    data['train'] = ds.concatenate_datasets(datas['train'])
+    data['valid'] = ds.concatenate_datasets(datas['valid'])
+    data['test'] = ds.concatenate_datasets(datas['test'])
 
     # Save the train and test datasets to disk
     Path(output_folder).mkdir(parents=True, exist_ok=True)
